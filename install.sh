caf74d508cd93c1fed20dfd785ac7387073667cc
Daniel Gustaw <gustaw.daniel@gmail.com>
2023-03-14 20:51:26 +0900

    install file updated probably it fix https://github.com/gustawdaniel/gpt-cli/issues/3

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 URL="$(wget -qO- https://api.github.com/repos/gustawdaniel/gpt-cli/releases/latest |\
- jq -r '.assets[] | select(.name | startswith("gpt-cli") and endswith("tar.gz")).browser_download_url')"
+ jq -r '.assets[] | select(.name=="gpt-cli").browser_download_url')"
 
 echo "$URL";
-wget "${URL}" -O /tmp/gtp-cli.tar.gz
+wget "${URL}" -O /tmp/gpt-cli
 cd /tmp && {
-  tar -zxvf gtp-cli.tar.gz;
-  sudo rm /usr/local/bin/gpt-cli /usr/local/bin/p
+  echo "Your password is required to save binary in /usr/local/bin"
+  sudo rm -f /usr/local/bin/gpt-cli /usr/local/bin/p
   sudo mv gpt-cli /usr/local/bin/gpt-cli;
+  sudo chmod +x /usr/local/bin/gpt-cli;
   sudo ln -s /usr/local/bin/gpt-cli /usr/local/bin/p;
+  sudo chmod +x /usr/local/bin/p;
   cd - || exit;
 }
