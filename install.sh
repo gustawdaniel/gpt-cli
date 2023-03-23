b7804724307c61f30db9fc4349d4d19630a44d69
Daniel Gustaw <gustaw.daniel@gmail.com>
2023-03-23 18:11:07 +0900

    musl for alpine

--- install.sh
+++ install.sh
@@ -4,30 +4,33 @@ FALLBACK_RELEASE_URL=https://api.github.com/repos/gustawdaniel/gpt-cli/releases/
 
 # Define a function that prints a message
 function download_binary {
+  BIN_SELECTOR=".assets[] | select(.name==\"gpt-cli.$([ "$PKG_MANAGER" = "apk" ] && echo "musl" || echo "gnu")\").browser_download_url"
+  SUM_SELECTOR=".assets[] | select(.name==\"gpt-cli.$([ "$PKG_MANAGER" = "apk" ] && echo "musl" || echo "gnu").sha256.txt\").browser_download_url"
+
   URL="$(wget -qO- https://api.github.com/repos/gustawdaniel/gpt-cli/releases/latest |
-    jq -r '.assets[] | select(.name=="gpt-cli.gnu").browser_download_url')"
+    jq -r "${BIN_SELECTOR}")"
 
   URL_SUM="$(wget -qO- https://api.github.com/repos/gustawdaniel/gpt-cli/releases/latest |
-    jq -r '.assets[] | select(.name=="gpt-cli.gnu.sha256.txt").browser_download_url')"
+    jq -r "${SUM_SELECTOR}")"
 
   if [ -z "$URL" ]; then
     echo "Fallback release"
     URL="$(wget -qO- ${FALLBACK_RELEASE_URL} |
-      jq -r '.assets[] | select(.name=="gpt-cli.gnu").browser_download_url')"
+      jq -r "${BIN_SELECTOR}")"
 
     URL_SUM="$(wget -qO- ${FALLBACK_RELEASE_URL} |
-      jq -r '.assets[] | select(.name=="gpt-cli.gnu.sha256.txt").browser_download_url')"
+      jq -r "${SUM_SELECTOR}")"
   fi
 
   echo "$URL"
-  wget "${URL}" -O /tmp/gpt-cli.gnu
-  wget "${URL_SUM}" -O /tmp/gpt-cli.gnu.sha256.txt
+  wget "${URL}" -O /tmp/gpt-cli
+  wget "${URL_SUM}" -O /tmp/gpt-cli.sha256.txt
 
   # Read the expected checksum from the file
-  EXPECTED_CHECKSUM=$(cat /tmp/gpt-cli.gnu.sha256.txt)
+  EXPECTED_CHECKSUM=$(cat /tmp/gpt-cli.sha256.txt)
 
   # Compute the actual checksum of the binary
-  ACTUAL_CHECKSUM=$(shasum -a 256 /tmp/gpt-cli.gnu | cut -d " " -f 1)
+  ACTUAL_CHECKSUM=$(shasum -a 256 /tmp/gpt-cli | cut -d " " -f 1)
 
   # Compare the checksums and display an error message if they differ
   if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]; then
@@ -43,51 +46,35 @@ function download_binary {
 function compile_binary {
   rm -rf target
   cargo build --target x86_64-unknown-linux-gnu --release
-  cp target/x86_64-unknown-linux-gnu/release/gpt-cli /tmp/gpt-cli.gnu
+  cp target/x86_64-unknown-linux-gnu/release/gpt-cli /tmp/gpt-cli
 }
 
-function install_os_dependencies {
+function set_pkg_manager {
   if [ -f /etc/os-release ]; then
     . /etc/os-release
     case $ID in
-      debian|ubuntu|raspbian)
-        PKG_MANAGER="apt"
-        echo "Installation ${PKG_MANAGER} dependencies"
-        sudo apt install jq libdigest-sha-perl libxcb1-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev -y
-        ;;
-      fedora)
-        PKG_MANAGER="dnf"
-        echo "Installation ${PKG_MANAGER} dependencies"
-        sudo dnf install jq perl-Digest-SHA libxcb -y
-        ;;
-      centos|rhel)
-        PKG_MANAGER="yum"
-        echo "Installation ${PKG_MANAGER} dependencies"
-        sudo yum install jq perl-Digest-SHA libxcb -y
-        ;;
-      opensuse|opensuse-leap|suse)
-        PKG_MANAGER="zypper"
-        echo "Installation ${PKG_MANAGER} dependencies"
-        sudo zypper --non-interactive install jq
-#        libxcb
-        ;;
-      arch|artix|manjaro)
-        PKG_MANAGER="pacman"
-        echo "Installation ${PKG_MANAGER} dependencies"
-        sudo pacman -Syu --noconfirm jq libxcb perl
-        PATH="${PATH:+${PATH}:}/usr/bin/core_perl"
-        ;;
-      alpine)
-        PKG_MANAGER="apk"
-        echo "Installation ${PKG_MANAGER} dependencies"
-        sudo apk add jq libxcb perl-utils
-#        PATH="${PATH:+${PATH}:}/usr/bin/core_perl"
-        which shasum
-        ;;
-      *)
-        echo "Unknown distribution, cannot determine the package manager"
-        exit 1
-        ;;
+    debian | ubuntu | raspbian)
+      PKG_MANAGER="apt"
+      ;;
+    fedora)
+      PKG_MANAGER="dnf"
+      ;;
+    centos | rhel)
+      PKG_MANAGER="yum"
+      ;;
+    opensuse | opensuse-leap | suse)
+      PKG_MANAGER="zypper"
+      ;;
+    arch | artix | manjaro)
+      PKG_MANAGER="pacman"
+      ;;
+    alpine)
+      PKG_MANAGER="apk"
+      ;;
+    *)
+      echo "Unknown distribution, cannot determine the package manager"
+      exit 1
+      ;;
     esac
   else
     echo "Cannot determine the distribution, please check /etc/os-release"
@@ -95,10 +82,41 @@ function install_os_dependencies {
   fi
 }
 
+function install_os_dependencies {
+  case $PKG_MANAGER in
+  apt)
+    echo "Installation ${PKG_MANAGER} dependencies"
+    sudo apt install jq libdigest-sha-perl libxcb1-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev -y
+    ;;
+  dnf)
+    echo "Installation ${PKG_MANAGER} dependencies"
+    sudo dnf install jq perl-Digest-SHA libxcb -y
+    ;;
+  yum)
+    echo "Installation ${PKG_MANAGER} dependencies"
+    sudo yum install jq perl-Digest-SHA libxcb -y
+    ;;
+  zypper)
+    echo "Installation ${PKG_MANAGER} dependencies"
+    sudo zypper --non-interactive install jq perl-Digest-SHA
+    ;;
+  pacman)
+    echo "Installation ${PKG_MANAGER} dependencies"
+    sudo pacman -Syu --noconfirm jq libxcb perl
+    PATH="${PATH:+${PATH}:}/usr/bin/core_perl"
+    ;;
+  apk)
+    echo "Installation ${PKG_MANAGER} dependencies"
+    sudo apk add jq libxcb perl-utils
+    ;;
+  esac
+}
+
 if [ "$1" = "--local" ]; then
   echo "Compilation started."
   compile_binary
 else
+  set_pkg_manager
   install_os_dependencies
   echo "Downloading started"
   download_binary
@@ -107,7 +125,7 @@ fi
 cd /tmp && {
   echo "Your password is required to save binary in /usr/local/bin"
   sudo rm -f /usr/local/bin/gpt-cli /usr/local/bin/p
-  sudo mv gpt-cli.gnu /usr/local/bin/gpt-cli
+  sudo mv gpt-cli /usr/local/bin/gpt-cli
   sudo chmod +x /usr/local/bin/gpt-cli
   sudo ln -s /usr/local/bin/gpt-cli /usr/local/bin/p
   sudo chmod +x /usr/local/bin/p
